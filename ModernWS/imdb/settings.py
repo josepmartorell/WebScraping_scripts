516450666b15b486bf5332cacbe62c98ac19cc70
josepmartorell <martorelljosep>
2020-06-06 22:08:18 +0200

    upload

--- ModernWS/imdb/settings.py
+++ ModernWS/imdb/settings.py
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'imdb.pipelines.IMDBPipeline': 300
-# }
+ITEM_PIPELINES = {
+   'imdb.pipelines.SQLlitePipeline': 300
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
